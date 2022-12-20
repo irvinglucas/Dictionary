818baad378df595aef6efc3ebcc1ed416bd7ff95
Irving Lucas <irving.lucas@gmail.com>
2022-12-20 19:10:24 -0300

    Added a splash screen to the app: a really cool API that came with android 12 release.

--- app/src/main/java/br/com/irvinglucas/dictionary/MainActivity.kt
+++ app/src/main/java/br/com/irvinglucas/dictionary/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.irvinglucas.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.com.irvinglucas.dictionary.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
